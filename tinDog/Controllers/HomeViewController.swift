199201c58dbe29e2595bb9f6400fb8e12e30ee7a
3lv27 <elvin.gm27@gmail.com>
2018-05-22 00:45:13 +0200

    [progress] login page added

--- tinDog/Controllers/HomeViewController.swift
+++ tinDog/Controllers/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RevealingSplashView
 
 class NavigationImageView : UIImageView {
     override func sizeThatFits(_ size: CGSize) -> CGSize {
@@ -19,20 +20,41 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var homeWrapper: UIStackView!
     @IBOutlet weak var likedImage: UIImageView!
     @IBOutlet weak var nopeImage: UIImageView!
+    let revealingSplashScreen = RevealingSplashView(iconImage: UIImage(named:"splash_icon")!, iconInitialSize: CGSize(width: 80, height: 80), backgroundColor: UIColor.white)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Setting Splash screen animation
+        self.view.addSubview(self.revealingSplashScreen)
+        self.revealingSplashScreen.animationType = SplashAnimationType.popAndZoomOut
+        self.revealingSplashScreen.startAnimation()
         
         let titleView = NavigationImageView()
         titleView.image = UIImage(named: "Actions")
         self.navigationItem.titleView = titleView
         let homeGR = UIPanGestureRecognizer(target: self, action: #selector(cardDragged(gestureRecongnizer: )))
         self.cardView.addGestureRecognizer(homeGR)
-        // Do any additional setup after loading the view.
+        
+        let userLoginBtn = UIButton(type: .custom)
+        userLoginBtn.setImage(UIImage(named: "login"), for: .normal)
+        userLoginBtn.imageView?.contentMode = .scaleAspectFit
+        userLoginBtn.addTarget(self, action: #selector(goToLogin(sender:)), for: .touchUpInside)
+        let userLoginBarBtn = UIBarButtonItem(customView: userLoginBtn)
+        self.navigationItem.leftBarButtonItem = userLoginBarBtn
+       
     }
     
+    @objc func goToLogin(sender: UIButton) {
+        let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let loginViewController = storyBoard.instantiateViewController(withIdentifier: "loginVC")
+        present(loginViewController, animated: true, completion: nil)
+        
+    }
+    
+    
     @objc func cardDragged(gestureRecongnizer: UIPanGestureRecognizer) {
-//        print("Drag\(gestureRecongnizer.translation(in: view))")
+
         let cardPoint = gestureRecongnizer.translation(in: view)
         self.cardView.center = CGPoint(x: self.view.bounds.width / 2 + cardPoint.x, y: self.view.bounds.height / 2 + cardPoint.y)
         
@@ -56,7 +78,7 @@ class HomeViewController: UIViewController {
         
         
         if gestureRecongnizer.state == .ended {
-//            print(self.cardView.center.x)
+
             if self.cardView.center.x < (self.view.bounds.width / 2 - 100) {
                 print("Dislike")
             }
