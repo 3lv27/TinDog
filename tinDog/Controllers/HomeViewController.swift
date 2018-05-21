086ce7012f47e33b44e62007b188ebe2d776709c
3lv27 <elvin.gm27@gmail.com>
2018-05-21 21:55:16 +0200

    [progress] rotation plus scale of the card

--- tinDog/Controllers/HomeViewController.swift
+++ tinDog/Controllers/HomeViewController.swift
@@ -33,6 +33,14 @@ class HomeViewController: UIViewController {
 //        print("Drag\(gestureRecongnizer.translation(in: view))")
         let cardPoint = gestureRecongnizer.translation(in: view)
         self.cardView.center = CGPoint(x: self.view.bounds.width / 2 + cardPoint.x, y: self.view.bounds.height / 2 + cardPoint.y)
+        
+        let xFromCenter = self.view.bounds.width / 2 - self.cardView.center.x
+        var rotate = CGAffineTransform(rotationAngle: xFromCenter / 200)
+        let scale = min(100 / abs(xFromCenter), 1)
+        var finalTransform = rotate.scaledBy(x: scale, y: scale)
+        
+        self.cardView.transform = finalTransform
+        
         if gestureRecongnizer.state == .ended {
 //            print(self.cardView.center.x)
             if self.cardView.center.x < (self.view.bounds.width / 2 - 100) {
@@ -42,6 +50,10 @@ class HomeViewController: UIViewController {
             if self.cardView.center.x > (self.view.bounds.width / 2 + 100) {
                 print("Like")
             }
+            
+            rotate = CGAffineTransform(rotationAngle: 0)
+            finalTransform = rotate.scaledBy(x: 1, y: 1)
+            self.cardView.transform = finalTransform
             
             self.cardView.center = CGPoint(x: self.homeWrapper.bounds.width / 2, y: (self.homeWrapper.bounds.height / 2 - 30))
         }
